aa2a86a1ae24b2b82892a62f830798969f3b2781
lis5662 <lis5662@gmail.com>
2019-07-03 22:52:44 +0300

    commit

--- requests_pars.py
+++ requests_pars.py
@@ -8,7 +8,6 @@ gamenews_url = 'https://www.igromania.ru/tech/articles/'
 def igromania_parse(gamenews_url):
     request = requests.get(gamenews_url)
     soup = bs(request.content, 'html.parser')
-
     print(soup)
 
 igromania_parse(gamenews_url)
