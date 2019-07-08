cb0c49d2c1a4ae4d13899df803672d7c9a9ab887
lis5662 <lis5662@gmail.com>
2019-07-08 23:29:38 +0300

    update

---
+++ python_bootcamp/iterators_generators/infinite.py
@@ -0,0 +1,26 @@
+#def current_beat():
+#    max = 100
+#    nums = (1,2,3,4)
+#    i = 0
+#    result = []
+#    while len(result) < max:
+#        if i >= len(nums): i = 0
+#        result.append(nums[i])
+#        i += 1
+#    return result
+
+# print(current_beat())
+
+def current_beat():
+    nums = (1,2,3,4)
+    i = 0
+    while True:
+        if i >= len(nums): i = 0
+        yield nums[i]
+        i += 1
+
+
+
+# def play_kick_drum():
+#    if current_beat() == 1:
+#            play_sound()
