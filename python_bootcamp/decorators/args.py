fdfab0e9581d6297bb801016c1d0f057bb8f2e0e
lis5662 <lis5662@gmail.com>
2019-07-09 22:24:22 +0300

    update

---
+++ python_bootcamp/decorators/args.py
@@ -0,0 +1,16 @@
+# NOT WORKING CODE!
+
+# When we write:
+@decorator
+def func(*args, **kwargs):
+    pass
+# We're really doing:
+func = decorator(func)
+
+# When we write:
+@decorator_with_args(arg)
+def func(*args, **kwargs):
+    pass
+
+# We're really doing:
+func = decorator_with_args(arg)(func)
