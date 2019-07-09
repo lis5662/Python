23b5b94ca2b6341da0a716f4f6f61cbc46ea1773
lis5662 <lis5662@gmail.com>
2019-07-09 09:05:53 +0300

    update

---
+++ python_bootcamp/iterators_generators/fib.py
@@ -0,0 +1,21 @@
+def fib_list(max):
+    nums = []
+    a, b = 0, 1
+    while len(nums) < max:
+        nums.append(b)
+        a, b = b, a + b
+    return nums
+
+print(fib_list(10))
+
+def fib_gen(max):
+    x = 0
+    y = 1
+    count = 0
+    while count < max:
+        x, y = y, x + y
+        yield x
+        count += 1
+
+for n in fib_list(1000000):
+    print(n)
