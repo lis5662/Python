74ae0a4f1d46c90567d65f132016cc6ec14ee321
lis5662 <lis5662@gmail.com>
2019-07-24 17:54:54 +0300

    update

---
+++ python_bootcamp/File _IO/write.py
@@ -0,0 +1,7 @@
+#with open("haiku.txt", "w") as file:
+#    file.write("Writing files is great\n")
+#    file.write("Here's another line of text\n")
+#    file.write("Closing now, goodbye!\n")
+
+with open("lol.txt", "w") as file:
+    file.write("haha" * 10000)
