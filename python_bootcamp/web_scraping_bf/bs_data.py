0bd2f5197290394ec3b0d0af95d0875449efa026
lis5662 <lis5662@gmail.com>
2019-07-29 15:10:18 +0300

    up

---
+++ python_bootcamp/web_scraping_bf/bs_data.py
@@ -0,0 +1,31 @@
+from bs4 import BeautifulSoup
+html = """
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>First HTML Page</title>
+</head>
+<body>
+  <div id="first">
+    <h3 data-example="yes">hi</h3>
+    <p>more text.</p>
+  </div>
+  <ol>
+    <li class="special">This list item is special.</li>
+    <li class="special">This list item is also special.</li>
+    <li>This list item is not special.</li>
+  </ol>
+  <div data-example="yes">bye</div>
+</body>
+</html>
+"""
+
+soup = BeautifulSoup(html, "html.parser")
+attr = soup.find("div")["id"]
+print(attr)
+# for el in soup.select(".special"):
+#    print(el.name)
+#    print(el.attrs['class'])
+# print(el)
+# print(d)
